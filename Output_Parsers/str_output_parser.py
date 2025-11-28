be6762220b7ac6ae6061e75e6c07386fed366211
Divya Sain <saindivya77@gmail.com>
2025-11-28 17:46:53 +0530

    adding output parsers

--- Output_Parsers/str_output_parser.py
+++ Output_Parsers/str_output_parser.py
@@ -1,9 +1,32 @@
-from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
 load_dotenv()
-llm=HuggingFaceEndpoint(repo_id="Tinylambda/Tinylambda-7B-Chat",
+import os
+from langchain_core.output_parsers import StrOutputParser
+"""llm=HuggingFaceEndpoint(repo_id="TinyLlama/TinyLlama-1.1B-Chat-v1.0",
                         task="text-generation")
 model=ChatHuggingFace(llm=llm)
+"""
+model=ChatOpenAI(
+    model="gryphe/mythomax-l2-13b",
+    openai_api_key=os.getenv("OPENROUTER_API_KEY"),
+    openai_api_base="https://openrouter.ai/api/v1"
+)
+#1st prompt-> detailed report
+template1=PromptTemplate(
+    template="write a detialed report about {topic}",   input_variables=["topic"]
+)
+
+#2nd prompt-> summary of the report
+template2=PromptTemplate(
+    template="summarize the following report in 5 lines:\n\n{report}",   input_variables=["report"]
+)
+parser=StrOutputParser()
+
+chain=template1|model|parser|template2|model|parser
+result=chain.invoke({"topic":"artificial intelligence"})
+print(result)
 
 
 
