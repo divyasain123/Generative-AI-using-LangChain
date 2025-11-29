2cec41dce6ce25270e5ed7dd8e18a1537c826967
Divya Sain <saindivya77@gmail.com>
2025-11-29 17:44:14 +0530

    add

--- Output_Parsers/struc_outputparser.py
+++ Output_Parsers/struc_outputparser.py
@@ -1,20 +1,33 @@
+from langchain.output_parsers import StructuredOutputParser, ResponseSchema
+from langchain_core.prompts import PromptTemplate
 from langchain_openai import ChatOpenAI
 from dotenv import load_dotenv
-from langchain_core.output_parsers import StructuredOutputParser, ResponseSchema
-
-
-
-load_dotenv()
 import os
 
-model=ChatOpenAI(
-    model="gryphe/mythomax-l2-13b", 
+load_dotenv()
+
+model = ChatOpenAI(
+    model="gryphe/mythomax-l2-13b",
     openai_api_key=os.getenv("OPENROUTER_API_KEY"),
     openai_api_base="https://openrouter.ai/api/v1"
 )
-struc_parser=StructuredOutputParser.from_names_and_types(
-    names_and_types={
-        "name": "str",
-        "age": "int"
-    }       
+
+schema = [
+    ResponseSchema(name='name1', description='one fact about the fictional character'),
+    ResponseSchema(name='name2', description='one fact about the fictional character'),
+    ResponseSchema(name='name3', description='one fact about the fictional character'),
+]
+
+parser = StructuredOutputParser.from_response_schemas(schema)
+
+template1 = PromptTemplate(
+    template="write a fact about {topic}\n{formal_instructions}",
+    input_variables=["topic"],
+    partial_variables={"formal_instructions": parser.get_format_instructions()},
 )
+
+chain = template1 | model | parser
+parsed = chain.invoke({"topic": "harry potter"})
+
+print(parsed)
+print(type(parsed))
