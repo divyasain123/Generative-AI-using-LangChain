d31afd47ea0e5aeac1758a11755b55df08eb801c
Divya Sain <saindivya77@gmail.com>
2025-12-07 18:19:20 +0530

    add

--- Runnables/runnables_passthrough.py
+++ Runnables/runnables_passthrough.py
@@ -2,8 +2,7 @@ from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
-from langchain_core.runnables import RunnableSequence
-
+from langchain_core.runnables import RunnableSequence, RunnableParallel, RunnablePassthrough
 
 load_dotenv()
 
@@ -21,6 +20,13 @@ prompt2 = PromptTemplate(
     input_variables=['text']
 )
 
-chain = RunnableSequence(prompt1, model, parser, prompt2, model, parser)
+joke_gen_chain = RunnableSequence(prompt1, model, parser)
 
-print(chain.invoke({'topic':'AI'}))
+parallel_chain = RunnableParallel({
+    'joke': RunnablePassthrough(),
+    'explanation': RunnableSequence(prompt2, model, parser)
+})
+
+final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
+
+print(final_chain.invoke({'topic':'cricket'}))
